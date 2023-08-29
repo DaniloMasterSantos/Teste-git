a849923ddafbb1bec6989b8a0370e52cdef6c883
DaniloMasterSantos <134393487+DaniloMasterSantos@users.noreply.github.com>
2023-08-29 13:29:28 -0300

    Lorem adiconado em Texto

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>TITULO TESTE</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt exercitationem fugit aliquam enim ut. Quae quasi asperiores eos odio, explicabo consectetur natus nisi accusamus iusto libero quod tempora eius reiciendis!</p>
 </body>
 </html>
